a7d7d5bf497e2826e01e486437cbd0f39289e030
Himanshu Mishra <mishrahimanshu2000@gmail.com>
2026-05-28 01:47:35 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        g = defaultdict(list)
+        indegree = [0] * numCourses 
+        for u,v in prerequisites:
+            g[v].append(u)
+            indegree[u] += 1
+        q = deque()
+        vis = [False] * numCourses
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+                vis[i] = True
+        taken = len(q)
+        while q:
+            u = q.popleft()
+            for v in g[u]:
+                indegree[v] -= 1
+                if indegree[v] == 0:
+                    q.append(v)
+                    taken += 1
+        
+        return taken == numCourses
+                
